7f2a2d46d3bd5c5355bbbffa354dccee7990101d
squesadav <sebas.quesada790@gmail.com>
2020-06-21 12:51:21 -0600

    Terminado paquete record_publishing

--- scripts/packages/app/record_publishing.sql
+++ scripts/packages/app/record_publishing.sql
@@ -5,24 +5,41 @@
 CREATE OR REPLACE PACKAGE record_publishing IS
     /* Returns a cursor of rows from Record with the given values, if given, in ascending creation date order.
        For example, if no values are given all rows are returned in the cursor */
+    FUNCTION with_numberr(vnumberr INTEGER) RETURN sys_refcursor;
     FUNCTION filter_records_by(vdate_crime DATE DEFAULT NULL,
                                vdate_expiration DATE DEFAULT NULL,
                                vid_type INTEGER DEFAULT NULL,
                                vid_veredict INTEGER DEFAULT NULL,
                                vid_person INTEGER DEFAULT NULL,
                                vid_community INTEGER DEFAULT NULL,
-                               vid_district INTEGER DEFAULT NULL) RETURN sys_refcursor;
+                               vid_district INTEGER DEFAULT NULL,
+                               vid_city INTEGER DEFAULT NULL,
+                               vid_state INTEGER DEFAULT NULL,
+                               vid_country INTEGER DEFAULT NULL) RETURN sys_refcursor;
 END record_publishing;
 /
 
 CREATE OR REPLACE PACKAGE BODY record_publishing IS
+    FUNCTION with_numberr(vnumberr INTEGER) RETURN sys_refcursor
+    AS 
+        crecord sys_refcursor;
+        BEGIN
+            OPEN crecord FOR
+                SELECT *
+                FROM record
+                WHERE numberr = vnumberr;
+            RETURN crecord;
+        END with_numberr;
     FUNCTION filter_records_by(vdate_crime DATE DEFAULT NULL,
                                vdate_expiration DATE DEFAULT NULL,
                                vid_type INTEGER DEFAULT NULL,
                                vid_veredict INTEGER DEFAULT NULL,
                                vid_person INTEGER DEFAULT NULL,
                                vid_community INTEGER DEFAULT NULL,
-                               vid_district INTEGER DEFAULT NULL) RETURN sys_refcursor
+                               vid_district INTEGER DEFAULT NULL,
+                               vid_city INTEGER DEFAULT NULL,
+                               vid_state INTEGER DEFAULT NULL,
+                               vid_country INTEGER DEFAULT NULL) RETURN sys_refcursor
     AS
         crecord sys_refcursor;
         BEGIN
@@ -40,7 +57,34 @@ CREATE OR REPLACE PACKAGE BODY record_publishing IS
                       (id_person = 
                         (SELECT id FROM person WHERE id_community = 
                             (SELECT id FROM community WHERE id_district = NVL(vid_district, id_district) OR id_district IS NULL) 
-                        OR id_community IS NULL) 
+                         OR id_community IS NULL)
+                       OR id_person IS NULL) AND
+                      (id_person = 
+                        (SELECT id FROM person WHERE id_community = 
+                            (SELECT id FROM community WHERE id_district = 
+                                (SELECT id FROM district WHERE id_city = NVL(vid_city, id_city) OR id_city IS NULL)
+                             OR id_district IS NULL) 
+                         OR id_community IS NULL)
+                       OR id_person IS NULL) AND
+                      (id_person = 
+                        (SELECT id FROM person WHERE id_community = 
+                            (SELECT id FROM community WHERE id_district = 
+                                (SELECT id FROM district WHERE id_city = 
+                                    (SELECT id FROM city WHERE id_state = NVL(vid_state, id_state) OR id_state IS NULL)
+                                 OR id_city IS NULL)
+                             OR id_district IS NULL) 
+                         OR id_community IS NULL)
+                       OR id_person IS NULL) AND
+                      (id_person = 
+                        (SELECT id FROM person WHERE id_community = 
+                            (SELECT id FROM community WHERE id_district = 
+                                (SELECT id FROM district WHERE id_city = 
+                                    (SELECT id FROM city WHERE id_state = 
+                                        (SELECT id FROM state WHERE id_country = NVL(vid_country, id_country) OR id_country IS NULL) 
+                                     OR id_state IS NULL)
+                                 OR id_city IS NULL)
+                             OR id_district IS NULL) 
+                         OR id_community IS NULL)
                        OR id_person IS NULL)
                 ORDER BY creation_date ASC;
             RETURN crecord;
@@ -54,7 +98,7 @@ declare
     l_numberr record.numberr%type;
 
 begin
-    mycursor := record_publishing.filter_records_by;
+    mycursor := record_publishing.filter_records_by(vdate_expiration => DATE '2028-10-05');
 
     loop
         fetch mycursor into l_numberr;
