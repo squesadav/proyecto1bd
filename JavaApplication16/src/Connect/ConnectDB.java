f939030b95ce0b33e8cc679dad40db0096bb2000
Sebasqc23 <62163037+Sebasqc23@users.noreply.github.com>
2020-06-24 00:00:23 -0600

    GetUserType and GetUserAll

--- JavaApplication16/src/Connect/ConnectDB.java
+++ JavaApplication16/src/Connect/ConnectDB.java
@@ -28,6 +28,28 @@ public class ConnectDB {
         stmnt.setString(1, name);
         stmnt.execute();
     }
+    
+        public static void getUserType(String name) throws SQLException {
+        String host = "jdbc:oracle:thin:@localhost:1521:PROYECTO1";
+        String uName = "ADM";
+        String uPass = "ADM";
+        //Lo dejo así porque no se si hay una funcion que devuelva cursor con todos los datos de user
+        //Voy a suponer que esa funcion si existe
+        Connection con = DriverManager.getConnection(host, uName, uPass);
+        CallableStatement stmnt = con.prepareCall("{ ? = call adminUser.getUserType(?) } ");
+        //CallableStatement stmnt = con.prepareCall("{ ? = call adminUser.getUserAll(?)}");
+     
+        stmnt.registerOutParameter(1, OracleTypes.Number);
+        //stmnt.registerOutParameter(1, OracleTypes.Cursor);
+        //stmnt.executeQuery(); creo que es igual a stmnt.execute();
+        //ResultSet user = (ResultSet) stmnt.getObject(1);
+        //string password = user.getString("password");
+        //int idType = (int) user.getString("id_type");
+        stmnt.setString(2, user_name);
+        stmnt.execute();
+        int typeUser = (int) stmnt.getObject(1);
+        
+    }
 }
 
 
---
+++ JavaApplication16/src/Connect/getUser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connect;
+
+/**
+ *
+ * @author Sebas
+ */
+public class getUser 
+{
+    ConnectDB.getUserType("sebasElRicoVelluti");
+    
+}
