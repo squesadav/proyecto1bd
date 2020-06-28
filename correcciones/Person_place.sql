cc115a2d4fdbe6e35fdd193fe08c196bb5d7faec
Sebasqc23 <62163037+Sebasqc23@users.noreply.github.com>
2020-06-28 15:44:43 -0600

    Update all

--- correcciones/Person_place.sql
+++ correcciones/Person_place.sql
@@ -9,34 +9,33 @@ ALTER TABLE person DROP CONSTRAINT fk_person_community;
 
 ALTER TABLE person DROP COLUMN id_community;
 
-ALTER TABLE person ADD COLUMN id_district;
+ALTER TABLE person ADD id_district NUMBER(4) NOT NULL;
 
 ALTER TABLE person
     ADD CONSTRAINT fk_person_district FOREIGN KEY(id_district) 
     references district(id);
 
 
-
 ALTER TABLE place DROP CONSTRAINT fk_place_community;
 
 ALTER TABLE place DROP COLUMN id_community;
 
-ALTER TABLE place ADD COLUMN id_district;
+ALTER TABLE place ADD id_district NUMBER(4) NOT NULL;
 
 ALTER TABLE place
-    ADD CONSTRAINT fk_person_district FOREIGN KEY(id_district) 
+    ADD CONSTRAINT fk_place_district FOREIGN KEY(id_district) 
     references district(id);
 
 
-ALTER TABLE person DROP CONSTRAINT pk_community;
+ALTER TABLE community DROP CONSTRAINT pk_community;
 
 
-ALTER TABLE person DROP CONSTRAINT fk_community_district;
+ALTER TABLE community DROP CONSTRAINT fk_community_district;
 
 DROP TABLE community;
 
 
-ALTER TABLE record ADD COLUMN id_district;
+ALTER TABLE record ADD id_district NUMBER(4) NOT NULL;
 
 ALTER TABLE record
     ADD CONSTRAINT fk_record_district FOREIGN KEY(id_district) 
